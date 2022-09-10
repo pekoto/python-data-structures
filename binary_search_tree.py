b6526d636724873acdb9276f31fb1f8b9d484fea
Graham <112996548+pekoto@users.noreply.github.com>
2022-09-10 15:13:00 -0400

    Add binary search tree

---
+++ binary_search_tree.py
@@ -0,0 +1,239 @@
+from typing import Any
+
+
+class Node:
+    """A class that represents a node in a Binary Search Tree."""
+
+    def __init__(self, key: Any, val: Any, count: int) -> None:
+        self.key = key
+        self.val = val
+        self.count = count
+
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    """A Binary Search Tree implementation.
+
+    A tree with symmetric order:
+        - Every element in the left subtree is < parent.
+        - Every element in the right subtree is > parent.
+
+    This means we can do binary searches since we know the direction
+    to go (left or right) at each step.
+
+    Operations typically take O(log n) time, but can take (n)
+    time if values are inserted with a natural ordering (then we
+    end up with a linked list).
+
+    Uses:
+        - Find k elements < value
+        - Quick lookup/insert (if elements randomly ordered)
+        - Print out min > max values (via inorder traversal)
+
+    Operations:
+        - Add
+        - Get
+        - Remove
+        - Size
+        - Max
+        - Min
+        _ Less than
+        - Traversals
+    """
+
+    def __init__(self) -> None:
+        """Initializes the Binary Search Tree."""
+        self._root = None
+
+    @property
+    def root(self) -> Node:
+        return self._root
+
+    def add(self, key: Any, val: Any) -> None:
+        """Adds a new node to the BST.
+
+        1. If the node is empty, add it.
+        2. If the node is < key, put it in left subtree.
+        3. If the node is > key, put in the right subtree.
+        4. If the  node is == key, just update the value.
+        5. Update the count.
+        6. Return the count.
+
+                 2
+                /  \
+               1    3
+        > add(0, 0)
+
+                 2
+                /  \
+               1    3
+              /
+             0
+
+        :param key: The key to add.
+        :param val: The value to add.
+
+        Time: O(log n) avg, O(n) worst.
+        """
+        self._root = self._add(self._root, key, val)
+
+    def _add(self, node: Node, key: Any, val: Any) -> Node:
+        """Helper method to add a new node."""
+        if not node:
+            return Node(key, val, 1)
+
+        if key < node.key:
+            node.left = self._add(node.left, key, val)
+        elif key > node.key:
+            node.right = self._add(node.right, key, val)
+        else:
+            node.val = val
+
+        node.count = _size(node.left) + 1 + _size(node.right)
+
+        return node
+
+    def get(self, key: Any) -> Any:
+        """Returns the value for a given key.
+
+        1. Set the node to head.
+        2. While node...
+            - If key == node.key, return the val.
+            - If key < node.key, search left.
+            - If key > node.key, search right.
+
+        Time: O(log n) avg, O(n) worst.
+
+        :param key: The key to find in the BST.
+        """
+        node = self._root
+
+        while node:
+            if key == node.key:
+                return node.val
+            elif key < node.key:
+                node = node.left
+            else:
+                node = node.right
+
+        raise KeyError(f'Key not found: {key}')
+
+    def remove(self, key: Any) -> None:
+        """Removes a node from the binary search tree.
+
+        This is the most complex operation, as we need to ensure
+        the BST remains valid when removing a node.
+
+        1. If the key is smaller, we remove in the left subtree.
+        2. If the key is larger, we remove in the right subtree.
+        3. Once we find the node to remove, things get tricky...
+
+            - If the node has no right subtree, set node to left subtree,
+                and vice-versa.
+
+            Remove "1" (replaces with 1.5):
+              2
+             / \
+            1   3
+            \
+             1.5
+          (No left subtree, so right subtree must be next smallest)
+
+            - If the node has left and right subtrees, replace with the smallest
+                value in the right subtree.
+
+             Remove "7" (replaces with 6)
+                10
+               / \
+              7    12
+             / \
+            2   8
+               /
+              6
+               \
+               6.5
+
+        Time:
+            Average: O(n)
+            Worst: O(sqrt(n))
+
+        :param key: The key to remove.
+        """
+        self._root = self._remove(self._root, key)
+
+    def _remove(self, node: Node, key: Any) -> Node:
+        """Helper method to remove a node."""
+        if not node:
+            raise KeyError(f'Key not found: {key}.')
+
+        if key < node.key:
+            node.left = self._remove(node.left, key)
+        elif key > node.key:
+            node.right = self._remove(node.right, key)
+        else:
+            if not node.left:
+                # Node has no left subtree -- set to right subtree.
+                return node.right
+            elif not node.right:
+                # Node has no right subtree -- set to left subtree.
+                return node.left
+            else:
+                # Node has left and right subtrees.
+                node_to_delete = node
+
+                # Replace the node with smallest node in right subtree.
+                node = _min(node_to_delete.right)
+
+                # Set right to be right subtree with smallest node removed.
+                node.right = self._delete_min(node_to_delete.right)
+
+                # Set left to be the left subtree.
+                node.left = node_to_delete.left
+
+        node.count = _size(node.left) + 1 + _size(node.right)
+        return node
+
+    def _delete_min(self, node: Node) -> Node:
+        """Deletes the minimum node in a subtree.
+        Once we hit a leftmost node, the min node will be that tree's right node.
+        1. If the node in the left is null, return node.right.
+        2. Delete from the left subtree.
+        3. Update the count.
+          7
+         / \
+        2   8
+           /
+          6
+           \
+           6.5
+        :param node: The node to delete the min from.
+        :return: The subtree with the minimum node deleted.
+        """
+        if not node.left:
+            return node.right
+
+        node.left = self._delete_min(node.left)
+        node.count = _size(node.left) + 1 + _size(node.right)
+        return node
+
+    def size(self) -> int:
+        """Returns the size of the tree."""
+        return _size(self.root)
+
+
+def _size(node: Node) -> int:
+    """Returns 0 if node is null, else returns size of node."""
+    return 0 if not node else node.count
+
+
+def _min(node: Node) -> Node:
+    """Gets the minimum node from the tree"""
+    if not node:
+        raise Exception("Tried to get min on empty tree.")
+
+    while node.left:
+        node = node.left
+
+    return node
---
+++ binary_search_tree_test.py
@@ -0,0 +1,56 @@
+import unittest
+
+from binary_search_tree import BinarySearchTree
+
+
+class TestBinarySearchTree(unittest.TestCase):
+
+    def test_add(self):
+        binary_search_tree = BinarySearchTree()
+
+        binary_search_tree.add(2, 2)
+        binary_search_tree.add(1, 1)
+        binary_search_tree.add(3, 3)
+
+        self.assertEqual(3, binary_search_tree.size())
+
+    def test_get(self):
+        binary_search_tree = BinarySearchTree()
+
+        binary_search_tree.add(2, 'two')
+        binary_search_tree.add(1, 'one')
+        binary_search_tree.add(3, 'three')
+
+        self.assertEqual('two', binary_search_tree.get(2))
+        self.assertEqual('one', binary_search_tree.get(1))
+        self.assertEqual('three', binary_search_tree.get(3))
+
+    def test_remove(self):
+        binary_search_tree = BinarySearchTree()
+        binary_search_tree.add(10, "10")
+        binary_search_tree.add(5, "5")
+        binary_search_tree.add(1, "1")
+        binary_search_tree.add(20, "20")
+        binary_search_tree.add(50, "50")
+        binary_search_tree.add(7, "7")
+
+        binary_search_tree.remove(10)  # Removes root
+        binary_search_tree.remove(50)  # Removes max node
+        binary_search_tree.remove(1)   # Removes min node
+
+        with self.assertRaises(KeyError):
+            binary_search_tree.get(10)
+
+        with self.assertRaises(KeyError):
+            binary_search_tree.get(50)
+
+        with self.assertRaises(KeyError):
+            binary_search_tree.get(1)
+
+        self.assertEqual("5", binary_search_tree.get(5))
+        self.assertEqual("20", binary_search_tree.get(20))
+        self.assertEqual("7", binary_search_tree.get(7))
+
+
+if __name__ == '__main__':
+    unittest.main()
