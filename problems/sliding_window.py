c8dccbf342d7ebc6b121483ff0b34baf55cea41d
Graham <112996548+pekoto@users.noreply.github.com>
2022-10-16 14:13:57 -0400

    Add sliding window

--- problems/sliding_window.py
+++ problems/sliding_window.py
@@ -23,3 +23,33 @@ def max_sum_subarray(nums: List[int], k: int) -> int:
             start += 1
 
     return max_sum
+
+
+def smallest_sum_subarray(nums: List[int], k: int) -> int:
+    """
+    Given an array of positive numbers and a positive number ‘S,’
+    find the length of the smallest contiguous subarray whose sum is
+    greater than or equal to ‘S’. Return 0 if no such subarray exists.
+
+    Example 1:
+
+    Input: [2, 1, 5, 2, 3, 2], S=7
+    Output: 2
+    Explanation: The smallest subarray with a sum greater than or equal to '7' is [5, 2].
+    """
+    start = 0
+    sum_so_far = 0
+    smallest_window = len(nums)+1
+
+    for end in range(len(nums)):
+        sum_so_far += nums[end]
+
+        while sum_so_far >= k:
+            smallest_window = min(smallest_window, (end-start)+1)
+            sum_so_far -= nums[start]
+            start += 1
+
+    if smallest_window == len(nums)+1:
+        return 0
+
+    return smallest_window
--- problems/sliding_window_test.py
+++ problems/sliding_window_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from sliding_window import max_sum_subarray
+from sliding_window import max_sum_subarray, smallest_sum_subarray
 
 
 class SlidingWindowTest(unittest.TestCase):
@@ -8,6 +8,11 @@ class SlidingWindowTest(unittest.TestCase):
     def test_max_array_sum(self):
         self.assertEqual(9, max_sum_subarray([2, 1, 5, 1, 3, 2], 3))
         self.assertEqual(7, max_sum_subarray([2, 3, 4, 1, 5], 2))
+
+    def test_smallest_sum_subarray(self):
+        self.assertEqual(2, smallest_sum_subarray([2, 1, 5, 2, 3, 2], 7))
+        self.assertEqual(1, smallest_sum_subarray([2, 1, 5, 2, 8], 7))
+        self.assertEqual(3, smallest_sum_subarray([3, 4, 1, 1, 6], 8))
 
 
 if __name__ == '__main__':
