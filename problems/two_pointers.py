1f4ed495469274c1e8fc41d7b85a7c2f13171a2c
Graham <112996548+pekoto@users.noreply.github.com>
2022-10-30 16:34:10 -0400

    Add two pointers

---
+++ problems/two_pointers.py
@@ -0,0 +1,95 @@
+from typing import List
+from collections import deque
+
+
+def target_sum(nums: List[int], target: int) -> List[int]:
+    """
+    Given an array of sorted numbers and a target sum,
+    find a pair in the array whose sum is equal to the given target.
+
+    Write a function to return the indices of the two numbers (i.e. the pair)
+    such that they add up to the given target.
+    """
+    start = 0
+    end = len(nums)-1
+
+    while end > start:
+        sum = nums[start] + nums[end]
+        if sum == target:
+            return [start, end]
+        elif sum < target:
+            start += 1
+        else:
+            # sum > target
+            end -= 1
+
+    return [-1, -1]
+
+
+def remove_duplicates(nums: List[int]) -> int:
+    """
+    Given an array of sorted numbers, remove all duplicate number instances from it in-place,
+    such that each element appears only once. The relative order of the elements should be
+    kept the same and you should not use any extra space so that that the solution have a
+    space complexity of O(1).
+
+    Move all the unique elements at the beginning of the array and after moving return the
+    length of the subarray that has no duplicate in it.
+    """
+    if len(nums) <= 1:
+        return len(nums)
+
+    start = 0
+
+    for end in range(len(nums)):
+
+        if nums[end] != nums[start]:
+            start += 1
+            nums[start] = nums[end]
+
+    return start + 1
+
+
+def remove_key(nums: List[int], key: int) -> int:
+    """
+    ven an unsorted array of numbers and a target ‘key’,
+    remove all instances of ‘key’ in-place and return the new length of the array.
+    """
+    # Input: [11, 1, 2, 2, 1], Key=2, Output: 4
+    #                ^
+    #                       ^
+    start = 0
+
+    for end in range(len(nums)):
+        if nums[end] != key:
+            nums[start] = nums[end]
+            start += 1
+
+    return start
+
+
+def square_sorted_array(nums: List[int]) -> List[int]:
+    """
+    Given a sorted array, create a new array containing squares of all
+    the numbers of the input array in the sorted order.
+    """
+    # [-2, -1, 0, 2, 3]
+    #          ^
+    #          ^
+    left = 0
+    right = len(nums)-1
+
+    squares = deque()
+
+    while left <= right:
+        left_square = nums[left]**2
+        right_square = nums[right]**2
+
+        if right_square > left_square:
+            squares.appendleft(right_square)
+            right -= 1
+        else:
+            squares.appendleft(left_square)
+            left += 1
+
+    return list(squares)
---
+++ problems/two_pointers_test.py
@@ -0,0 +1,25 @@
+import unittest
+import two_pointers
+
+
+class TwoPointersTest(unittest.TestCase):
+
+    def test_two_pointers(self):
+        self.assertListEqual([1, 3], two_pointers.target_sum([1, 2, 3, 4, 6], 6))  # add assertion here
+        self.assertListEqual([0, 2], two_pointers.target_sum([2, 5, 9, 11], 11))  # add assertion here
+
+    def test_remove_duplicates(self):
+        self.assertEqual(4, two_pointers.remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+        self.assertEqual(2, two_pointers.remove_duplicates([2, 2, 2, 11]))
+
+    def test_remove_key(self):
+        self.assertEqual(4, two_pointers.remove_key([3, 2, 3, 6, 3, 10, 9, 3], 3))
+        self.assertEqual(2, two_pointers.remove_key([2, 11, 2, 2, 1], 2))
+
+    def test_square_sorted_array(self):
+        self.assertListEqual([0, 1, 4, 4, 9], two_pointers.square_sorted_array([-2, -1, 0, 2, 3]))
+        self.assertListEqual([0, 1, 1, 4, 9], two_pointers.square_sorted_array([-3, -1, 0, 1, 2]))
+
+
+if __name__ == '__main__':
+    unittest.main()
